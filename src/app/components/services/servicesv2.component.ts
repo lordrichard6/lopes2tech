788b988172f6752eec774ea489d5250795de1274
reizinho86 <paulolopesreizinho@gmail.com>
2025-09-19 13:03:14 +0100

    feat: Add service dialog background images and enhanced translations

--- src/app/components/services/servicesv2.component.ts
+++ src/app/components/services/servicesv2.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
 import { TranslatePipe } from '../../pipes/translate.pipe';
+import { ServiceDialogComponent } from './service-dialog/service-dialog.component';
 
 @Component({
   selector: 'app-servicesv2',
-  imports: [TranslatePipe],
+  imports: [TranslatePipe, ServiceDialogComponent],
   templateUrl: './servicesv2.component.html',
   styleUrl: './servicesv2.component.scss'
 })
 export class ServicesV2Component {
-  
+  isDialogOpen = false;
+  selectedServiceKey = '';
+
+  openServiceDialog(serviceKey: string): void {
+    this.selectedServiceKey = serviceKey;
+    this.isDialogOpen = true;
+  }
+
+  closeServiceDialog(): void {
+    this.isDialogOpen = false;
+    this.selectedServiceKey = '';
+  }
 }
